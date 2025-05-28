ce7e031c92381d2491866817212db60cb7028def
nndang <nndang.sc@gmail.com>
2025-05-28 20:28:22 +0700

    Add project files.

---
+++ QL_THU_VIEN_KG/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QL_THU_VIEN_KG.Startup))]
+namespace QL_THU_VIEN_KG
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
